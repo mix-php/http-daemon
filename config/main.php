9b9c6518700f82b2377253184551d07db25782ba
Jian Liu <onanying@126.com>
2019-01-17 09:53:26 +0800

    修改配置

--- config/main.php
+++ config/main.php
@@ -7,7 +7,7 @@ return [
     'appName'          => 'mix-httpd',
 
     // 应用版本
-    'appVersion'       => '1.0.0',
+    'appVersion'       => '2.0.1',
 
     // 应用调试
     'appDebug'         => false,
