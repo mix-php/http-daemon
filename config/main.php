bd84ffbba9b3694441b0e4cbb731690ea03d82e2
Jian Liu <onanying@126.com>
2019-03-29 18:10:34 +0800

    增加标准输出

--- config/main.php
+++ config/main.php
@@ -109,18 +109,18 @@ return [
             'properties' => [
                 // 日志记录级别
                 'levels'  => ['emergency', 'alert', 'critical', 'error', 'warning', 'notice', 'info', 'debug'],
-                // 处理者
+                // 处理器
                 'handler' => [
                     // 依赖引用
-                    'ref' => beanname(Mix\Log\FileHandler::class),
+                    'ref' => beanname(Mix\Log\StdoutHandler::class),
                 ],
             ],
         ],
 
-        // 处理者
+        // 日志标准输出处理器
         [
             // 类路径
-            'class' => Mix\Log\FileHandler::class,
+            'class' => Mix\Log\StdoutHandler::class,
         ],
 
     ],
